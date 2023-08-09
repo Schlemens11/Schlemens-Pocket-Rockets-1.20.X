af0c98bfb648df0792fd47f9e95d20c7a40fc201
Clemens Schneider <e12219440@student.tuwien.ac.at>
2023-08-09 19:13:15 +0200

    Initial commit

---
+++ src/main/java/net/schlemens/schlemenspocketrockets/SchlemensPocketRockets.java
@@ -0,0 +1,16 @@
+package net.schlemens.schlemenspocketrockets;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SchlemensPocketRockets implements ModInitializer {
+    public static final String MOD_ID = "schlemenspocketrockets";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
